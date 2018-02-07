7ac42b7eb7ab51ca84a6a942d61f70dbb7493d60
delak <dela.kumahor@gmail.com>
2018-02-07 08:31:42 +0000

    Upgraded dropdown menu etc.

--- assets/dak.js
+++ assets/dak.js
@@ -1,16 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+  var toggleMenu = function() {
+    $('.site-header').toggleClass('active-menu');
+    $('main').toggleClass('fader');
+    $('footer').toggleClass('fader');
+  };
+
+  $('.menu-toggle').click(function() {
+    toggleMenu();
+  });
+
+  $(".click-blocker").on("click", function() {
+    if($('.site-header').hasClass('active-menu')) {
+      toggleMenu();
+    };
+  });
+
   $(window).on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
-    var scroll_pos_test = $('.intro-container').height() + 15;
+    var scroll_pos_test = $('.intro-container').height() + $('.masthead').height()/2;
 
     if(y_scroll_pos > scroll_pos_test) {
       $('.site-header').addClass('orangeify');
     } else {
       $('.site-header').removeClass('orangeify');
     }
-  });
 
-  $('.menu-toggle').click(function() {
-    $('.project-thumbnails').slideToggle(200);
+    if($('.site-header').hasClass('active-menu')) {
+      toggleMenu();
+    }
   });
 });
