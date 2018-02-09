d5b67cb79cf4d95ec0ea337429c31ede9700d289
delak <dela.kumahor@gmail.com>
2018-02-09 09:17:32 +0000

    Dropdown menu refinements

--- assets/dak-home.js
+++ assets/dak-home.js
@@ -1,21 +1,57 @@
-$(document).ready(function(){
-  var p = $('.site-header');
-  var offset = p.offset().top;
+$(document).ready(function() {
+  var sh = $('.site-header');
+  var m = $('.masthead')
+  var hb = $('.hello-bio');
+  var hs = $('.hello-section');
+  var offset = sh.offset().top;
+  var showTitle = false;
+
+  var toggleMenu = function() {
+    $('.site-header').toggleClass('active-menu');
+    $('main').toggleClass('fader');
+    $('footer').toggleClass('fader');
+    if (showTitle == false) {
+      sh.toggleClass('show-title');
+    }
+  };
+
+  $('.menu-toggle').click(function() {
+    toggleMenu();
+  });
+
+  $(".click-blocker").on("click", function() {
+    if($('.site-header').hasClass('active-menu')) {
+      toggleMenu();
+    };
+  });
 
   $(window).on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
-    var scroll_pos_test = $('.hello-section').height() - 45;
+    var scroll_pos_title = hb.position().top + hb.outerHeight(true) - 30;
+    var scroll_pos_test = hs.height();
 
     if(y_scroll_pos > offset) {
-      $('.site-header').addClass('fixed-header');
+      sh.addClass('fixed-header');
     } else {
-      $('.site-header').removeClass('fixed-header');
+      sh.removeClass('fixed-header');
+    }
+
+    if(y_scroll_pos > scroll_pos_title) {
+      showTitle = true;
+      sh.addClass('show-title');
+    } else {
+      showTitle = false;
+      sh.removeClass('show-title');
     }
 
     if(y_scroll_pos > scroll_pos_test) {
-      $('.site-header').addClass('orangeify');
+      sh.addClass('orangeify');
     } else {
-      $('.site-header').removeClass('orangeify');
+      sh.removeClass('orangeify');
+    }
+
+    if($('.site-header').hasClass('active-menu')) {
+      toggleMenu();
     }
   });
 });
