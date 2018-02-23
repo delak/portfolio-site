2f72cb42217eaf396d5919350afc439aac3e660f
delak <dela.kumahor@gmail.com>
2018-02-23 08:00:17 +0000

    New color scheme etc.

--- assets/dak.js
+++ assets/dak.js
@@ -18,11 +18,5 @@ $(document).ready(function() {
   $(window).on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos_test = $('.intro-container').height() + $('.masthead').height()/2;
-
-    if(y_scroll_pos > scroll_pos_test) {
-      $('.site-header').addClass('orangeify');
-    } else {
-      $('.site-header').removeClass('orangeify');
-    }
   });
 });
