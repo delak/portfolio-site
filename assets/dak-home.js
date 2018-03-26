ccbe83156e895d616ae9e047688bb245dd4a38d6
delak <dela.kumahor@gmail.com>
2018-03-26 06:34:40 +0000

    big changes

--- assets/dak-home.js
+++ assets/dak-home.js
@@ -1,27 +1,72 @@
 $(document).ready(function() {
   var sh = $('.site-header');
-  var hb = $('.hello-bio');
   var hs = $('.hello-section');
-  var mt = $('.masthead-title');
-  var showTitle = false;
+  var mt = $('.menu-toggle');
+  var mf = $('.menu-footer');
+  var mh = $('.menu-header');
+  var pc = $('.projects-container');
+  var pp = $('.projects-panel');
+  var win = $(window);
+  var allowHeaderScroll;
+
+  //function that toggles the menu
+  var toggleMenu = function() {
+    sh.toggleClass('active-menu');
+    if(sh.hasClass('active-menu')) {
+      mf.css({'top' : (win.height() - mf.height()) + 'px'});
+    } else {
+      mf.css({'top' : win.height() + 'px'});
+    };
+
+    if(allowHeaderScroll == true) { // if close to the header, scroll to the top of the page
+      $("html, body").animate({ scrollTop: 0 }, 200, function() {
+        allowHeaderScroll = false;
+      });
+      return false;
+    };
+  };
+
+  // controls the menu button
+  mt.click(function() {
+    toggleMenu();
+  });
+
+  // exit from the menu if user clicks anywhere on the background
+  $(".click-blocker").on("click", function() {
+    if(sh.hasClass('active-menu')) {
+      toggleMenu();
+    };
+  });
 
   $(window).on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
-    var scroll_pos_title = hb.position().top + hb.outerHeight(true) - 30;
-    var scroll_pos_test = hs.height();
+    var panel_pos = pc.position();
+    var scroll_pos_test = hs.height() - 30;
 
-    if(y_scroll_pos > scroll_pos_title) {
-      showTitle = true;
-      mt.addClass('show-title');
+    if(y_scroll_pos > panel_pos.top) {
+      pp.addClass('panel-fix');
     } else {
-      showTitle = false;
-      mt.removeClass('show-title');
+      pp.removeClass('panel-fix');
     }
 
     if(y_scroll_pos > scroll_pos_test) {
       sh.addClass('orangeify');
     } else {
       sh.removeClass('orangeify');
+    }
+
+    if(y_scroll_pos < 60) {
+      mh.addClass('deactivate');
+      if(y_scroll_pos > 5) {
+        allowHeaderScroll = true;
+      }
+    } else {
+      mh.removeClass('deactivate');
+      allowHeaderScroll = false;
+    }
+
+    if(sh.hasClass('active-menu') && (allowHeaderScroll == false)) {
+      toggleMenu();
     }
   });
 });
--- assets/dak.js
+++ assets/dak.js
@@ -1,20 +1,82 @@
 $(document).ready(function() {
-  $('.menu-toggle').click(function() {
+  var cs = $('.case-study');
+  var mt = $('.menu-toggle');
+  var sh = $('.site-header');
+  var mf = $('.menu-footer');
+  var mh = $('.menu-header');
+  var win = $(window);
+  var allowFooterScroll;
+  var allowHeaderScroll;
+
+  //function that toggles the menu
+  var toggleMenu = function() {
+    sh.toggleClass('active-menu');
+    if(sh.hasClass('active-menu')) {
+      mf.css({'top' : (win.height() - mf.height()) + 'px'});
+    } else {
+      mf.css({'top' : win.height() + 'px'});
+    };
+    if(allowFooterScroll == true) { // if close to the footer, scroll to the bottom of the page
+      $("html, body").animate({ scrollTop: $(document).height() }, "slow", function() {
+        allowFooterScroll = false;
+      });
+      return false;
+    };
+
+    if(allowHeaderScroll == true) { // if close to the header, scroll to the top of the page
+      $("html, body").animate({ scrollTop: 0 }, 200, function() {
+        allowHeaderScroll = false;
+      });
+      return false;
+    };
+  };
+
+  // controls the menu button
+  mt.click(function() {
     toggleMenu();
   });
 
-  $('.info-pane').hover(function() {
-    var hoverImage = $(this).attr('data-image');
-    $('#image'+hoverImage).toggleClass('hover');
+  // exit from the menu if user clicks anywhere on the background
+  $(".click-blocker").on("click", function() {
+    if(sh.hasClass('active-menu')) {
+      toggleMenu();
+    };
   });
 
+  //make sure that videos loop forever
   $('video').on('ended', function () {
     this.load();
     this.play();
   });
 
+
   $(window).on('scroll', function() {
-    var y_scroll_pos = window.pageYOffset;
-    var scroll_pos_test = $('.intro-container').height() + $('.masthead').height()/2;
+    var y_scroll_pos = win.scrollTop();
+    var page_height = win.height();
+    var scroll_pos_footer = cs.position().top + cs.outerHeight(true);
+
+    if((y_scroll_pos + page_height) > scroll_pos_footer) {            // if user has scrolled past the case study
+      mf.addClass('deactivate');                                      // disable the fixed footer
+      if((y_scroll_pos + page_height) < $(document).height() - 5) {
+        allowFooterScroll = true;
+      }
+    } else {                                                         // but if user is scrolling within the case study
+      mf.removeClass('deactivate');                                  // enable the fixed footer
+      allowFooterScroll = false;
+    }
+
+    if(y_scroll_pos < 60) {
+      mh.addClass('deactivate');
+      if(y_scroll_pos > 5) {
+        allowHeaderScroll = true;
+      }
+    } else {
+      mh.removeClass('deactivate');
+      allowHeaderScroll = false;
+    }
+
+    if(sh.hasClass('active-menu') && (allowFooterScroll == false) && (allowHeaderScroll == false)) {
+      toggleMenu();
+    }
   });
 });
