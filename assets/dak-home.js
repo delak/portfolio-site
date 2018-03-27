3a6eb436ce7d2d5c4259f3776c3f67d95f2b5a76
delak <dela.kumahor@gmail.com>
2018-03-27 18:10:35 +0000

    Touch device fixes + new menu

--- assets/dak-home.js
+++ assets/dak-home.js
@@ -1,72 +1,23 @@
 $(document).ready(function() {
   var sh = $('.site-header');
   var hs = $('.hello-section');
-  var mt = $('.menu-toggle');
-  var mf = $('.menu-footer');
-  var mh = $('.menu-header');
-  var pc = $('.projects-container');
-  var pp = $('.projects-panel');
   var win = $(window);
   var allowHeaderScroll;
 
-  //function that toggles the menu
-  var toggleMenu = function() {
-    sh.toggleClass('active-menu');
-    if(sh.hasClass('active-menu')) {
-      mf.css({'top' : (win.height() - mf.height()) + 'px'});
-    } else {
-      mf.css({'top' : win.height() + 'px'});
-    };
+  // function that binds hover to click fo touch devices
+  $('.hover').bind('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });
 
-    if(allowHeaderScroll == true) { // if close to the header, scroll to the top of the page
-      $("html, body").animate({ scrollTop: 0 }, 200, function() {
-        allowHeaderScroll = false;
-      });
-      return false;
-    };
-  };
-
-  // controls the menu button
-  mt.click(function() {
-    toggleMenu();
-  });
-
-  // exit from the menu if user clicks anywhere on the background
-  $(".click-blocker").on("click", function() {
-    if(sh.hasClass('active-menu')) {
-      toggleMenu();
-    };
-  });
-
-  $(window).on('scroll', function() {
+  win.on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
-    var panel_pos = pc.position();
     var scroll_pos_test = hs.height() - 30;
 
-    if(y_scroll_pos > panel_pos.top) {
-      pp.addClass('panel-fix');
-    } else {
-      pp.removeClass('panel-fix');
-    }
-
     if(y_scroll_pos > scroll_pos_test) {
-      sh.addClass('orangeify');
+      sh.addClass('bg-whitener');
     } else {
-      sh.removeClass('orangeify');
-    }
-
-    if(y_scroll_pos < 60) {
-      mh.addClass('deactivate');
-      if(y_scroll_pos > 5) {
-        allowHeaderScroll = true;
-      }
-    } else {
-      mh.removeClass('deactivate');
-      allowHeaderScroll = false;
-    }
-
-    if(sh.hasClass('active-menu') && (allowHeaderScroll == false)) {
-      toggleMenu();
+      sh.removeClass('bg-whitener');
     }
   });
 });
