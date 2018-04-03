98dd2a4b542158400795e1f0d49175fc444b55dc
delak <dela.kumahor@gmail.com>
2018-04-03 15:59:29 +0000

    Updated snippet thumbnails

--- assets/dak-home.js
+++ assets/dak-home.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   var sh = $('.site-header');
   var hs = $('.hello-section');
   var snippet = $('.snippet');
+  var snippet_clicker = $('.snippet-body, .snippet .title-wrapper .project-question');
+  var pl = $('.snippet .project-label');
   var hb = $('.hamburger');
   var win = $(window);
 
@@ -9,22 +11,30 @@ $(document).ready(function() {
     sh.toggleClass('toggle-menu');
   });
 
-  snippet.click(function() {
-    $(this).toggleClass('toggle-preview').siblings().removeClass('toggle-preview');
+  $.fn.vAlign = function() {
+    return this.each(function(i){
+      var ah = $(this).outerHeight();
+      $(this).css('margin-top', (ah * -1));
+    });
+  };
+
+  $('.snippet .project-label').vAlign();
+
+  snippet_clicker.click(function() {
+    $(this).closest('.snippet').toggleClass('toggle-snippet');
+    snippet.not($(this).closest('.snippet')).removeClass('toggle-snippet');
   });
 
   win.on("load resize scroll", function(e) {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos_test = hs.height() - 30;
 
-    if(y_scroll_pos > scroll_pos_test) {
-      sh.addClass('bg-whitener');
-    } else {
-      sh.removeClass('bg-whitener');
-    }
-
     if(sh.hasClass('toggle-menu')) {
       sh.removeClass('toggle-menu');
     }
   });
+
+  win.on("resize", function(e) {
+    $('.snippet .project-label').vAlign();
+  })
 });
