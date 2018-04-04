ce033c4d624979821ce0563c50045daa2d3434eb
delak <dela.kumahor@gmail.com>
2018-04-04 19:51:55 +0000

    - technical issues resolved - colors resolved etc.

--- assets/dak-home.js
+++ assets/dak-home.js
@@ -5,27 +5,41 @@ $(document).ready(function() {
   var snippet_clicker = $('.snippet-body, .snippet .title-wrapper .project-question');
   var pl = $('.snippet .project-label');
   var hb = $('.hamburger');
-  var win = $(window);
 
   hb.click(function() {
     sh.toggleClass('toggle-menu');
   });
 
   $.fn.vAlign = function() {
-    return this.each(function(i){
-      var ah = $(this).outerHeight();
-      $(this).css('margin-top', (ah * -1));
+    return this.each(function(){
+      $(this).css('margin-top', ($(this).outerHeight() * -1));
+    });
+  };
+
+  $.fn.hideMe = function() {
+    var thisSnippet = $(this);
+    return this.each(function() {
+      thisSnippet.removeClass('snippet-show');
+      setTimeout(function() {
+        thisSnippet.removeClass('animatable');
+      }, 400);
     });
   };
 
   $('.snippet .project-label').vAlign();
 
   snippet_clicker.click(function() {
-    $(this).closest('.snippet').toggleClass('toggle-snippet');
-    snippet.not($(this).closest('.snippet')).removeClass('toggle-snippet');
+    var thisSnippet = $(this).closest('.snippet');
+    thisSnippet.find('.project-label').vAlign();
+    if (thisSnippet.hasClass('snippet-show')) {
+      snippet.hideMe();
+    } else {
+      thisSnippet.addClass('animatable snippet-show');
+      snippet.not(thisSnippet).hideMe();
+    }
   });
 
-  win.on("load resize scroll", function(e) {
+  $(window).on("load resize scroll", function(e) {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos_test = hs.height() - 30;
 
@@ -33,8 +47,4 @@ $(document).ready(function() {
       sh.removeClass('toggle-menu');
     }
   });
-
-  win.on("resize", function(e) {
-    $('.snippet .project-label').vAlign();
-  })
 });
